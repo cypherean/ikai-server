69cbd6e1f50dcc1f54f5888e13e91336c9e73069
burnerlee <avi.aviral140@gmail.com>
2020-10-04 18:27:08 +0530

    update database settings

--- ikaiServer/settings.py
+++ ikaiServer/settings.py
@@ -87,9 +87,12 @@ WSGI_APPLICATION = 'ikaiServer.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'xvqvottc',
+        'USER': 'xvqvottc',
+        'PASSWORD': 'bUy-UVUdbtLxRZNxSOmzGNKaX9qee5Cn',
+        'HOST': 'arjuna.db.elephantsql.com',
+        'PORT': '5432'}
 }
 
 
